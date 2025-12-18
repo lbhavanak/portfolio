99b32dabcea8a4747ea0be20ffff72d389bd1f98
lbhavanak <karrilakshmibhavana@gmail.com>
2025-12-18 20:23:37 +0530

    Update portfolio sections and navbar

--- client/client/src/components/Navbar.jsx
+++ client/client/src/components/Navbar.jsx
@@ -14,23 +14,11 @@ export default function Navbar() {
       <h2 className="logo">My Portfolio</h2>
 
       <ul className="nav-links">
-        <li>
-          <a onClick={() => scrollToSection("home")}>Home</a>
-        </li>
-
-        <li>
-          <a onClick={() => scrollToSection("about")}>About</a>
-        </li>
-
-        <li>
-          <a onClick={() => scrollToSection("projects")}>Projects</a>
-        </li>
-
-        <li>
-          <a onClick={() => scrollToSection("contact")}>Contact</a>
-        </li>
+        <li><a onClick={() => scrollToSection("home")}>Home</a></li>
+        <li><a onClick={() => scrollToSection("about")}>About</a></li>
+        <li><a onClick={() => scrollToSection("projects")}>Projects</a></li>
+        <li><a onClick={() => scrollToSection("contact")}>Contact</a></li>
       </ul>
     </nav>
   );
 }
-
